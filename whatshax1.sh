8f2d7b4393c847aba277d37a5b9361e390e4a618
dzhax499 <57004084+dzhax499@users.noreply.github.com>
2019-11-05 20:35:27 +0700

    Add files via upload

---
+++ whatshax1.sh
@@ -0,0 +1,70 @@
+clear
+echo "
+       _                     _                 __ 
+      | |          _        | |               /  |
+ _ _ _| | _   ____| |_   ___| | _   ____ _   /_/ |
+| | | | || \ / _  |  _) /___) || \ / _  ( \ / )| |
+| | | | | | ( ( | | |__|___ | | | ( ( | |) X ( | |
+ \____|_| |_|\_||_|\___|___/|_| |_|\_||_(_/ \_)|_|
+                                                  "
+sleep 3
+echo "
+Author  = Mr.DzHax       IG/FB  = Dzakirtsabit
+Youtube = YtDzakir       Github = Tunggu Sampe 1kSubscriber oke!"
+sleep 3
+read -p "Siapa nick kaka? " nick;
+sleep 1
+echo "Selamat datang $nick !"
+sleep 2
+echo "Nomor Anda?"
+read -p "" no;                                                                                                          sleep 2
+echo "Nomor Target?"
+read -p "" nomor;
+sleep 3
+echo "login with = $no"
+sleep 3
+echo "target with = $nomor"
+sleep 3
+echo "[+] Syncing Target..."
+sleep 3
+echo "[+] Mencoba Cracking..."
+sleep 3
+echo "[+] Berhasil!"
+get_url=$(curl -s https://Dzhax.com/)
+sleep 4
+echo "Getting URL..."
+sleep 4
+echo "Buat verify code anda disini Harus 6 angka!"
+read -p "Verify Code => " code;
+sleep 3
+echo "Code ini membutuhkan waktu 5-10 menit"
+sleep 3
+echo "Masuk ke Google..."
+ifconfig
+sleep 3
+echo "melakukan cracking.."
+sleep 10
+curl -s https://dzhax.com/
+echo "YOUR CONECTION NOT AVAIBLE!"
+sleep 3
+echo "Kode [$code] di Masukan..."
+sleep 15
+echo "Hacking Cyber DzHax..."
+sleep 10
+echo "Scanning..."
+sleep 12
+echo "Anda Belum Melakukan Hacking SebelumNya ✔"
+sleep 3
+echo "Pastikan Nomor Target Aktif/Tidak Hangus"
+sleep 3
+echo "nomor $nomor Tidak hangus ✔"
+sleep 3
+echo "Exploiting..."
+sleep 12
+echo "Sukses ✔"
+sleep 3
+curl -s https://v.whatsapps.com/$code
+sleep 4
+echo "YOUR LINK IS UNDETECTED : https://v.whatsapps.com/$code/"
+echo "must subscribe me [YtDzakir]"
+exit
